d5dc5514b7ecc6d90f5041eeebdb75b78e893fce
sushantzunjare2005-sudo <sushantzunjare2005@gmail.com>
2026-03-23 05:35:30 -0700

    Add files via upload

---
+++ exp no 1 sushant.py
@@ -0,0 +1,31 @@
+   #integer
+Roll_no=262
+print(type(Roll_no))
+
+#float
+b=10.5
+print(type(b))
+
+#string
+print(type(c))
+
+#boolean
+d=True
+print(type(d))
+
+#list
+e=[1,2,3,"python"]
+print(type(e))
+
+#tuple
+f=(10,20,30)
+print(type(f))
+
+#dictionary
+g={"name":"dev",
+   "age":20}
+print(type(g))
+
+#set
+h={1,2,3,4}
+print(type(h))
